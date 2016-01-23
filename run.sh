c94b2b7179b377f4f2ae056e24d07837410c2fda
Ian Blenke <ian@blenke.com>
2016-01-23 01:57:19 -0500

    Config overrides

--- run.sh
+++ run.sh
@@ -4,11 +4,11 @@ export PATH=/newrelic/daemon/:/newrelic/scripts/:$PATH
 
 export NRSYSMOND_license_key=${NRSYSMOND_license_key:-$NEW_RELIC_LICENSE_KEY}
 export NRSYSMOND_loglevel=${NRSYSMOND_loglevel:-info}
-export NRSYSMOND_collector_host=${NRSYSMOND_collector_host:-collector.newrelic.com}
 export NRSYSMOND_ssl=${NRSYSMOND_ssl:-true}
 export NRSYSMOND_disable_docker=${NRSYSMOND_disable_docker:-false}
 export NRSYSMOND_disable_nfs=${NRSYSMOND_disable_nfs:-false}
 
+#NRSYSMOND_collector_host=${NRSYSMOND_collector_host:-collector.newrelic.com}
 #NRSYSMOND_labels=type:docker-host;cluster:coreos;region:useast;
 #NRSYSMOND_proxy=
 #NRSYSMOND_docker_connection=
@@ -20,7 +20,14 @@ export NRSYSMOND_disable_nfs=${NRSYSMOND_disable_nfs:-false}
 #NRSYSMOND_ssl_ca_path=
 #NRSYSMOND_cgroup_root=
 
-#nrsysmond-config --set license_key=${NEW_RELIC_LICENSE_KEY:-NRSYSMOND_license_key}
+env | grep NRSYSMOND | while read line ; do
+  key="$(echo $line | cut -d= -f1- | sed -e 's/^NRSYSMOND_//')
+  value="$(echo $line | cut -d= -f2-)"
+  nrsysmond-config --set $key="$value"
+done
+
+if [ -n "CAT_CONFIG" ]; then
+  cat /etc/newrelic/nrsysmond.cfg
+fi
 
 exec nrsysmond -c /etc/newrelic/nrsysmond.cfg -l /dev/stdout -f
-
